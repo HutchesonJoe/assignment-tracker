5faf56244e5f05b50fe5992588a557264ba918c6
Joe <hutcheson2001@yahoo.com>
2022-05-02 10:49:06 -0400

    add delete button

--- src/components/SubmissionsPage/allsubmissions/AllSubCard.js
+++ src/components/SubmissionsPage/allsubmissions/AllSubCard.js
@@ -7,12 +7,28 @@ function AllSubCard({submission, submissions, setSubs}){
   const [formOn, setFormOn] = useState(false)
 
   const thisSub = thisStudSubs.find((sub)=> sub.id === submission.id)
-  console.log(submission, submissions)
+
   function handleClick(){
     setFormOn(!formOn)
     fetch(`http://localhost:9292/submissions_by_student/${submission.student_id}`)
     .then(r=> r.json())
     .then(data=>setThisStudSubs(data))
+  }
+
+  function handleDelete(){
+    console.log(submission)
+    alert("Are you sure you want to delete this submission?")
+    fetch(`http://localhost:9292/submissions/${submission.id}`,{
+      method: "DELETE",
+      headers: { 
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then (r=>r.json())
+    .then ((data)=>{
+      const newSubs = submissions.filter(sub => data.id !== sub.id)
+      setSubs(newSubs)
+    })
   }
 
   return (
@@ -23,7 +39,7 @@ function AllSubCard({submission, submissions, setSubs}){
       <h4>{submission.teacher_notes}</h4>
       <div className="edit-delete-box">
         <button className="edit-option" onClick={handleClick}>Edit</button>
-        <button className="delete-option">Delete</button>
+        <button className="delete-option" onClick={handleDelete}>Delete</button>
         <div>{thisSub ? <EditForm thisSub={thisSub} setSubs={setSubs} submissions={submissions} /> : ""}</div>
       </div>
      
